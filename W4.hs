e83923e1e6669ff2e5a13364761370cc33141c32
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-05-15 20:29:00 +0300

    greet done

--- W4.hs
+++ W4.hs
@@ -31,7 +31,7 @@ hello = do
 -- argument and prints a line "HELLO name".
 
 greet :: String -> IO ()
-greet name = undefined
+greet name = putStrLn ("HELLO " ++ name)
 
 
 -- Ex 3: define the IO operation greet2 that reads a name from the
